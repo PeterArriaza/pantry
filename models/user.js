90472b94b14058185cef72dff04433b7438f9457
PeterArriaza <peter.arriaza@gmail.com>
2019-02-27 20:43:24 -0500

    added imgs to readme

---
+++ models/user.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    household: {
+        type: String,
+        required: false
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
