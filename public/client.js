5ff0ac78cc8b22cc8bf518c4940587bbd9b81273
Peter Arriaza <peter.arriaza@gmail.com>
2019-03-01 16:21:45 -0500

    added password validation for sign up form

--- public/client.js
+++ public/client.js
@@ -6,3 +6,10 @@ function togglePasswordVisibility() {
         x.type = "password";
     }
 }
+
+$('#new-password, #confirm-new-password').on('keyup', function () {
+    if ($('#new-password').val() == $('#confirm-new-password').val()) {
+      $('#password-validation-message').html('Passwords match!').css('color', 'green');
+    } else 
+      $('#password-validation-message').html('Passwords do not match.').css('color', 'red');
+  });
